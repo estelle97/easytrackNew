8ffa5e2a2ba21e5cb8f76bc1f77879c36569dc2a
Mr Wiltek <underclash94@gmail.com>
2020-08-06 18:25:57 +0100

    create-purchase-finished

--- app/Http/Controllers/Admin/PurchaseController.php
+++ app/Http/Controllers/Admin/PurchaseController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PurchaseResource;
+use App\Purchase;
+use App\Site;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PurchaseController extends Controller
 {
@@ -35,16 +40,70 @@ class PurchaseController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'site_id' => 'required',
+            'supplier_id' => 'required',
+            'order' => 'required',
+        ]);
+
+        do {
+            $code = mt_rand(1000000000, 9999999999);
+        } while(Purchase::whereCode($code)->exists());
+
+        $purchase = new Purchase([
+            'code' => $code,
+            'status' => $request->status,
+            'site_id' => $request->site_id,
+            'supplier_id' => $request->supplier_id,
+            'initiator_id' => Auth::user()->id,
+            'paying_method' => $request->paying_method,
+            'shipping_cost' => $request->shipping_cost,
+            'purchase_text' => $request->purchase_text,
+        ]);
+
+        DB::transaction(function () use($request,$purchase){
+            $products = explode('|', rtrim($request->order,'|'));
+            $purchase->save();
+            foreach ($products as $prods) {
+                $prod = explode(';', $prods);
+                $purchase->products()->attach($prod[0],[
+                    'qty' => $prod[1],
+                    'cost' => $prod[2],
+                    'damages' => $prod[3],
+                    'site_id' => $request->site_id,
+                ]);
+            }
+        });
+
+
+        return "success";
+    }
+
+    public function getElementBySite(Request $request){
+        $suppliers = '';
+        $products = '';
+
+        foreach (Site::find($request->site_id)->suppliers as $supl) {
+            $suppliers .= "<option value='".$supl->id."'> ".$supl->name."</option>";
+        }
+
+        foreach (Site::find($request->site_id)->products as $prod) {
+            $products .= "<option class='product' data-id='".$prod->id."' data-qty='".$prod->pivot->qty."' data-price='".$prod->pivot->price."' value='".$prod->id."'> ".$prod->name."</option>";
+        }
+
+        return response()->json([
+            "suppliers" => $suppliers,
+            "products" => $products
+        ], 200);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Purchase  $purchase
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Purchase $purchase)
     {
         //
     }
@@ -52,10 +111,10 @@ class PurchaseController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Purchase  $purchase
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Purchase $purchase)
     {
         //
     }
@@ -64,10 +123,10 @@ class PurchaseController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Purchase  $purchase
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Purchase $purchase)
     {
         //
     }
@@ -75,10 +134,10 @@ class PurchaseController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Purchase  $purchase
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Purchase $purchase)
     {
         //
     }
