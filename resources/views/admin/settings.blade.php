12e338f9bb22bf34748cdd4aabd0a6b446a0b735
Steph DGenius <stephanetse@gmail.com>
2020-08-27 22:43:43 +0100

    ✨ Adding settings page

---
+++ resources/views/admin/settings.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.base')
+
+@section('content')
+
+<!-- Page title -->
+<div class="page-header-2 text-white">
+    <div class="row align-items-center">
+        <div class="col-auto mb-3">
+            <h2 class="page-title">
+                Mes paramètres
+            </h2>
+        </div>
+
+    </div>
+</div>
+<div class="row row-deck row-cards">
+
+
+</div>
+
+@endsection
+
+
+@section('scripts')
+
+
+@endsection
+
+@section('styles')
+    <link href={{asset("template/assets/dist/css/easytrak-payments.min.css")}} rel="stylesheet" />
+@endsection
