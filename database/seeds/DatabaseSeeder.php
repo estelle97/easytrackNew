969a2204d5c39ba53354da7b26ff4fe411e9da41
MrWiltek <underclash94@gmail.com>
2020-09-03 21:25:06 +0100

    remove products-categories fakers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         factory(\App\Company::class, 4)->create();
         factory(\App\Site::class, 30)->create();
         factory(\App\Supplier::class, 40)->create();
-        factory(\App\Category::class, 5)->create();
-        factory(\App\Product::class, 30)->create();
+        // factory(\App\Category::class, 5)->create();
+        // factory(\App\Product::class, 30)->create();
         factory(\App\Type::class, 4)->create();
 
         $this->call([
@@ -48,33 +48,33 @@ class DatabaseSeeder extends Seeder
         });
 
         // Replissage de la table product_site
-        foreach (App\Site::all() as $site) {
+        // foreach (App\Site::all() as $site) {
 
-            $products = App\Product::all('id')->random(rand(10,25));
-            foreach ($products as $prod) {
-                $price = rand(4,20)*150;
-                $cost = $price - $price * 0.1;
-                $site->products()->attach($prod->id,[
-                    'price' => $price,
-                    'cost' => $cost,
-                    'qty' => rand(20, 30),
-                    'qty_alert' => 5,
-                ]);
-            }
+        //     $products = App\Product::all('id')->random(rand(10,25));
+        //     foreach ($products as $prod) {
+        //         $price = rand(4,20)*150;
+        //         $cost = $price - $price * 0.1;
+        //         $site->products()->attach($prod->id,[
+        //             'price' => $price,
+        //             'cost' => $cost,
+        //             'qty' => rand(20, 30),
+        //             'qty_alert' => 5,
+        //         ]);
+        //     }
 
-            $suppliers = App\Supplier::all()->random(rand(10,20));
-            foreach ($suppliers as $supl) {
-                $supl->site_id = $site->id;
-                $supl->save();
-            }
+        //     $suppliers = App\Supplier::all()->random(rand(10,20));
+        //     foreach ($suppliers as $supl) {
+        //         $supl->site_id = $site->id;
+        //         $supl->save();
+        //     }
 
-            App\Customer::create([
-                'name' => 'Passager',
-                'street' => $site->street,
-                'town' => $site->town,
-                'site_id' => $site->id
-            ]);
-        };
+        //     App\Customer::create([
+        //         'name' => 'Passager',
+        //         'street' => $site->street,
+        //         'town' => $site->town,
+        //         'site_id' => $site->id
+        //     ]);
+        // };
 
 
     }
