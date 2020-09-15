00fa254ad59d8b4b7c61f0eabe14a3580b763010
Mr Wiltek <underclash94@gmail.com>
2020-09-15 08:23:14 +0100

    chat- getRecipients method finished

--- app/Message.php
+++ app/Message.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Message extends Model
 {
@@ -11,4 +12,28 @@ class Message extends Model
         'sender', 'receiver', 'message', 'status', 'file'
     ];
     protected $table = 'messages';
+
+
+    public static function getRecipients(){
+
+        if(Auth::user()->is_admin == 1){
+            return $users = Auth::user()->employee->site->employees;
+        }
+
+        if(Auth::user()->is_admin == 3){
+            return $users = \App\User::all();
+        }
+
+        if(Auth::user()->is_admin == 2){
+            $users = [];
+           foreach(Auth::user()->companies->first()->sites as $sites){
+               foreach($sites->employees as $emp){
+                   $users[] = $emp;
+               }
+           }
+
+           return $users;
+        }
+
+    }
 }
