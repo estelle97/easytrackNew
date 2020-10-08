91f6ac4b46b61e1a1d57d07b20a99997830db27c
Steph DGenius <stephanetse@gmail.com>
2020-10-08 23:02:38 +0100

    💄 Update Profile menu links and minor fixes

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -76,17 +76,21 @@
                             @if (Auth::check())
                                 @if (Auth::user()->is_admin == 1)
                                     <div class="profile-menu d-none d-flex flex-column text-reset">
-                                        <div class="profile-info d-flex align-items-center">
+                                        <div class="profile-info d-flex align-items-center justify-content-end">
                                             <span class="mr-3" style="color: #000;">
-                                               {{Auth::user()->name}}
+                                               <span class="greeting">Hi,</span> {{Auth::user()->name}}
                                             </span>
                                             <img src="https://ui-avatars.com/api/?name={{Auth::user()->name}}&background=E4E4E4&color=7D7D7D&font-size=0.30" alt="" style="height: 45px; width: 45px; border-radius: 100px;">
                                         </div>
 
-                                        <div class="profile-sub-menu d-flex flex-column">
+                                        <div class="profile-sub-menu pt-2 d-flex flex-column">
                                             <a class="sub-menu-link" href="{{route('employee.dashboard')}}">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M21 20a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.49a1 1 0 0 1 .386-.79l8-6.222a1 1 0 0 1 1.228 0l8 6.222a1 1 0 0 1 .386.79V20zm-2-1V9.978l-7-5.444-7 5.444V19h14z"/></svg>
                                                 Tableau de bord
+                                            </a>
+                                            <a class="sub-menu-link" href="{{route('employee.notifications')}}">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M18 10a6 6 0 1 0-12 0v8h12v-8zm2 8.667l.4.533a.5.5 0 0 1-.4.8H4a.5.5 0 0 1-.4-.8l.4-.533V10a8 8 0 1 1 16 0v8.667zM9.5 21h5a2.5 2.5 0 1 1-5 0z"/></svg>
+                                                Notifications
                                             </a>
                                             <a class="sub-menu-link" href="{{route('employee.profile')}}">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
@@ -96,6 +100,7 @@
                                                 </svg>
                                                 Profle
                                             </a>
+                                            <hr class="mt-3 mb-3">
                                             <a class="sub-menu-link" href="{{route('logout')}}">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M5 22a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v3h-2V4H6v16h12v-2h2v3a1 1 0 0 1-1 1H5zm13-6v-3h-7v-2h7V8l5 4-5 4z"/></svg>
                                                 Se déconnecter
@@ -106,17 +111,21 @@
 
                                 @if (Auth::user()->is_admin == 2)
                                     <div class="profile-menu d-none d-flex flex-column text-reset">
-                                        <div class="profile-info d-flex align-items-center">
+                                        <div class="profile-info d-flex align-items-center justify-content-end">
                                             <span class="mr-3" style="color: #000;">
-                                            {{Auth::user()->name}}
+                                                <span class="greeting">Hi,</span> {{Auth::user()->name}}
                                             </span>
                                             <img src="https://ui-avatars.com/api/?name={{Auth::user()->name}}&background=E4E4E4&color=7D7D7D&font-size=0.30" alt="" style="height: 45px; width: 45px; border-radius: 100px;">
                                         </div>
 
-                                        <div class="profile-sub-menu d-flex flex-column">
+                                        <div class="profile-sub-menu pt-2 d-flex flex-column">
                                             <a class="sub-menu-link" href="{{route('admin.dashboard')}}">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M21 20a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.49a1 1 0 0 1 .386-.79l8-6.222a1 1 0 0 1 1.228 0l8 6.222a1 1 0 0 1 .386.79V20zm-2-1V9.978l-7-5.444-7 5.444V19h14z"/></svg>
                                                 Tableau de bord
+                                            </a>
+                                            <a class="sub-menu-link" href="{{route('admin.notifications')}}">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M18 10a6 6 0 1 0-12 0v8h12v-8zm2 8.667l.4.533a.5.5 0 0 1-.4.8H4a.5.5 0 0 1-.4-.8l.4-.533V10a8 8 0 1 1 16 0v8.667zM9.5 21h5a2.5 2.5 0 1 1-5 0z"/></svg>
+                                                Notifications
                                             </a>
                                             <a class="sub-menu-link" href="{{route('admin.profile')}}">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
@@ -126,10 +135,7 @@
                                                 </svg>
                                                 Profle
                                             </a>
-                                            <a class="sub-menu-link" href="{{route('admin.settings')}}">
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 1l9.5 5.5v11L12 23l-9.5-5.5v-11L12 1zm0 2.311L4.5 7.653v8.694l7.5 4.342 7.5-4.342V7.653L12 3.311zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/></svg>
-                                                Paramètres
-                                            </a>
+                                            <hr class="mt-3 mb-3">
                                             <a class="sub-menu-link" href="{{route('logout')}}">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M5 22a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v3h-2V4H6v16h12v-2h2v3a1 1 0 0 1-1 1H5zm13-6v-3h-7v-2h7V8l5 4-5 4z"/></svg>
                                                 Se déconnecter
@@ -140,14 +146,13 @@
 
                                 @if (Auth::user()->is_admin == 3)
                                     <div class="profile-menu d-none d-flex flex-column text-reset">
-                                        <div class="profile-info d-flex align-items-center">
+                                        <div class="profile-info d-flex align-items-center justify-content-end">
                                             <span class="mr-3" style="color: #000;">
-                                               {{Auth::user()->name}}
+                                               <span class="greeting">Hi,</span> {{Auth::user()->name}}
                                             </span>
                                             <img src="https://ui-avatars.com/api/?name={{Auth::user()->name}}&background=E4E4E4&color=7D7D7D&font-size=0.30" alt="" style="height: 45px; width: 45px; border-radius: 100px;">
                                         </div>
-
-                                        <div class="profile-sub-menu d-flex flex-column">
+                                        <div class="profile-sub-menu pt-2 d-flex flex-column">
                                             <a class="sub-menu-link" href="{{route('easytrack.dashboard')}}">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M21 20a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.49a1 1 0 0 1 .386-.79l8-6.222a1 1 0 0 1 1.228 0l8 6.222a1 1 0 0 1 .386.79V20zm-2-1V9.978l-7-5.444-7 5.444V19h14z"/></svg>
                                                 Tableau de bord
@@ -160,6 +165,11 @@
                                                 </svg>
                                                 Profle
                                             </a>
+                                            <a class="sub-menu-link" href="#">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7.253 18.494l.724.423A7.953 7.953 0 0 0 12 20a8 8 0 1 0-8-8c0 1.436.377 2.813 1.084 4.024l.422.724-.653 2.401 2.4-.655zM2.004 22l1.352-4.968A9.954 9.954 0 0 1 2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.954 9.954 0 0 1-5.03-1.355L2.004 22zM8.391 7.308c.134-.01.269-.01.403-.004.054.004.108.01.162.016.159.018.334.115.393.249.298.676.588 1.357.868 2.04.062.152.025.347-.093.537a4.38 4.38 0 0 1-.263.372c-.113.145-.356.411-.356.411s-.099.118-.061.265c.014.056.06.137.102.205l.059.095c.256.427.6.86 1.02 1.268.12.116.237.235.363.346.468.413.998.75 1.57 1l.005.002c.085.037.128.057.252.11.062.026.126.049.191.066a.35.35 0 0 0 .367-.13c.724-.877.79-.934.796-.934v.002a.482.482 0 0 1 .378-.127c.06.004.121.015.177.04.531.243 1.4.622 1.4.622l.582.261c.098.047.187.158.19.265.004.067.01.175-.013.373-.032.259-.11.57-.188.733a1.155 1.155 0 0 1-.21.302 2.378 2.378 0 0 1-.33.288 3.71 3.71 0 0 1-.125.09 5.024 5.024 0 0 1-.383.22 1.99 1.99 0 0 1-.833.23c-.185.01-.37.024-.556.014-.008 0-.568-.087-.568-.087a9.448 9.448 0 0 1-3.84-2.046c-.226-.199-.435-.413-.649-.626-.89-.885-1.562-1.84-1.97-2.742A3.47 3.47 0 0 1 6.9 9.62a2.729 2.729 0 0 1 .564-1.68c.073-.094.142-.192.261-.305.127-.12.207-.184.294-.228a.961.961 0 0 1 .371-.1z"/></svg>
+                                                Asssitance technique
+                                            </a>
+                                            <hr class="mt-3 mb-3">
                                             <a class="sub-menu-link" href="{{route('logout')}}">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M5 22a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v3h-2V4H6v16h12v-2h2v3a1 1 0 0 1-1 1H5zm13-6v-3h-7v-2h7V8l5 4-5 4z"/></svg>
                                                 Se déconnecter
